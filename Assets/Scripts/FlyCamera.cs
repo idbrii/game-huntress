9b38f88c8e38f1fdc043c9e42cc6cd5b97d864dc
David Briscoe <idbrii@gmail.com>
2018-01-28 08:37:53 -0800

    Update FlyCamera to use InputManager
    Use InputManager and its axes.
    
    Create new axis DebugCam_Altitude for adjusting height. Apply to code
    and Input prefab.

--- Assets/Scripts/FlyCamera.cs
+++ Assets/Scripts/FlyCamera.cs
@@ -4,8 +4,9 @@
 // Released under Creative Commons Attribution Share Alike.
 using UnityEngine;
 using System.Collections;
+using TeamUtility.IO;
  
-public class ExtendedFlycam : MonoBehaviour
+public class FlyCamera : MonoBehaviour
 {
  
 	/*
@@ -41,34 +42,33 @@ public class ExtendedFlycam : MonoBehaviour
  
 	void Update ()
 	{
-		rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-		rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+		rotationX += InputManager.GetAxis("LookHorizontal") * cameraSensitivity * Time.deltaTime;
+		rotationY += InputManager.GetAxis("LookVertical") * cameraSensitivity * Time.deltaTime;
 		rotationY = Mathf.Clamp (rotationY, -90, 90);
  
 		transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
 		transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
  
-	 	if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))
+	 	if (InputManager.GetKey (KeyCode.LeftShift) || InputManager.GetKey (KeyCode.RightShift))
 	 	{
-			transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-			transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+			transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * InputManager.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * InputManager.GetAxis("Horizontal") * Time.deltaTime;
 	 	}
-	 	else if (Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl))
+	 	else if (InputManager.GetKey (KeyCode.LeftControl) || InputManager.GetKey (KeyCode.RightControl))
 	 	{
-			transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-			transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+			transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * InputManager.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * InputManager.GetAxis("Horizontal") * Time.deltaTime;
 	 	}
 	 	else
 	 	{
-	 		transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-			transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+	 		transform.position += transform.forward * normalMoveSpeed * InputManager.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * normalMoveSpeed * InputManager.GetAxis("Horizontal") * Time.deltaTime;
 	 	}
  
  
-		if (Input.GetKey (KeyCode.Q)) {transform.position += transform.up * climbSpeed * Time.deltaTime;}
-		if (Input.GetKey (KeyCode.E)) {transform.position -= transform.up * climbSpeed * Time.deltaTime;}
+		transform.position += transform.up * climbSpeed * Time.deltaTime * InputManager.GetAxis("DebugCam_Altitude");
  
-		if (Input.GetKeyDown (KeyCode.End))
+		if (InputManager.GetKeyDown (KeyCode.End))
 		{
 			Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
 		}
